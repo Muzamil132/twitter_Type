97961a9fbac0cb2df3524977f9dd64802ac24c12
Muzamil132 <muzamil073@gmail.com>
2022-09-10 06:33:41 -0700

    5th

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react";
 import { RecoilRoot } from "recoil";
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component,  pageProps: { session, ...pageProps } }: AppProps) {
    
   return(
     <RecoilRoot>
-   <SessionProvider>
+   <SessionProvider session={session}  >
   <Component {...pageProps} />
   </SessionProvider>
     </RecoilRoot>
